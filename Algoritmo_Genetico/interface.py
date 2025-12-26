acfa673d3830b29b10bc37e9dfffdac72a0f2755
Thiago Plancke <thiagoplancke@gmail.com>
2025-12-26 19:50:04 -0300

    Update interface.py

--- Algoritmo_Genetico/interface.py
+++ Algoritmo_Genetico/interface.py
@@ -4,13 +4,13 @@ from random import randint
 
 pygame.init()
 
-# Tamanho da tela
+
 largura = 600
 altura = 600
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Algoritmo Genético - Visualização")
 
-# Cores
+
 BRANCO = (255,255,255)
 PRETO = (0,0,0)
 VERDE = (0,255,0)
@@ -18,13 +18,13 @@ AZUL = (0,0,255)
 
 
 def desenhar_individuos(populacao, objetivo):
-    tela.fill(BRANCO)  # limpa a tela
-    # Desenhar objetivo
-    pygame.draw.circle(tela, VERDE, (objetivo[0]*10, objetivo[1]*10), 8)  # multiplicando pra escala
+    tela.fill(BRANCO)  
+    
+    pygame.draw.circle(tela, VERDE, (objetivo[0]*10, objetivo[1]*10), 8)  
 
-    # Desenhar indivíduos
+    
     for ind in populacao:
-        pos = ind["posicao_final"]  # ou posicao se quiser mostrar início
+        pos = ind["posicao_final"]  
         pygame.draw.circle(tela, AZUL, (pos[0]*10, pos[1]*10), 5)
 
     pygame.display.update()
